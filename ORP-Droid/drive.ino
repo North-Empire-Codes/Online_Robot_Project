c068e1eaca95c97f98bc923ff66191bc612f589d
EmperoroftheNorth <areichnorris@gmail.com>
2014-11-30 18:19:34 -0800

    Create drive.ino

---
+++ ORP-Droid/drive.ino
@@ -0,0 +1,9 @@
+//This function sets up all of the Arduino ports.
+void setup(){
+  //Insert set up code here.
+}
+
+//This function loops until the Arduino loses power.
+void loop(){
+  //Insert looped code here.
+}
