ed9776823bd06a390c0d7e53441b57abf1aecda6
EmperoroftheNorth <areichnorris@gmail.com>
2014-12-18 23:48:06 -0800

    Delete drive.ino

--- ORP-Droid/drive.ino
+++
@@ -1,9 +0,0 @@
-//This function sets up all of the Arduino ports.
-void setup(){
-  //Insert set up code here.
-}
-
-//This function loops until the Arduino loses power.
-void loop(){
-  //Insert looped code here.
-}
