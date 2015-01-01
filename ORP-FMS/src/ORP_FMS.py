322a2972ea4ac3ad3ac45c090707b8385129cba9
EmperoroftheNorth <>
2014-12-31 19:08:02 -0800

    Starting the ORP-FMS directory with some Python code.

---
+++ ORP-FMS/src/ORP_FMS.py
@@ -0,0 +1,9 @@
+# To change this license header, choose License Headers in Project Properties.
+# To change this template file, choose Tools | Templates
+# and open the template in the editor.
+
+__author__="Emperor of the North"
+__date__ ="$Dec 31, 2014 Dec 31, 2014 7:05:38 PM$"
+
+if __name__ == "__main__":
+    print "Hello World"
