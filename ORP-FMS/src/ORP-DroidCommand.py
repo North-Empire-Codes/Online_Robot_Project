e5cd91882401d6c69b6fbf2d485dc6a967414503
Northern-Emperor <Northern-Emperor@users.noreply.github.com>
2015-01-17 15:36:16 -0800

    Update ORP-DroidCommand.py
    Making sure the code is updated to the most recent version.

--- ORP-FMS/src/ORP-DroidCommand.py
+++ ORP-FMS/src/ORP-DroidCommand.py
@@ -1,44 +1,40 @@
 #!/usr/bin/python
-
 #Imports Serial Stream module
-import SerialStream
+from SerialStream import *
 from time import sleep
-
 #Variables
-ORP-DroidChannel = "/dev/ttyACM0"
+ORPDroidChannel = "/dev/ttyACM0"
 frequency = 9600
-
 #Initializes stream on ORP-Droid COM port and frequency
-initStream(arduino, frequency)
-
+initStream(ORPDroidChannel, frequency)
 #Makes sure the port is open
 openStream()
-
 #Sends 3 command to ORP-Droid
-send(char("A"))
-sleep(2)
-send(char("D"))
-sleep(2)
-send(char("C"))
-sleep(2)
-send(char("F"))
-/*
-Command List
-A-A Left Forward
-B-A Left Backward
-C-A Left Stop
-D-A Right Forward
-E-A Right Backward
-F-A Right Stop
-U-B Left Forward
-V-B Left Backward
-W-B Left Stop
-X-B Right Forward
-Y-B Right BackWard
-Z-B Right Stop
-*/
+send("AAA")
+sleep(4)
+send("DDD")
+sleep(4)
+send("CCC")
+sleep(4)
+send("FFF")
+sleep(4)
+
+#
+#Command List
+#A-A Left Forward
+#B-A Left Backward
+#C-A Left Stop
+#D-A Right Forward
+#E-A Right Backward
+#F-A Right Stop
+#U-B Left Forward
+#V-B Left Backward
+#W-B Left Stop
+#X-B Right Forward
+#Y-B Right BackWard
+#Z-B Right Stop
+#
 
 #Closes Serial port
 closeStream()
-
 exit()
